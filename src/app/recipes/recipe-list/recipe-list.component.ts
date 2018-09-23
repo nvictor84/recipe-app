b75dec3dd20c4f2a2f9d1b765a7be1b5e76e3494
Victor Noguera <noguera.victor@gmail.com>
2018-09-23 15:51:28 -0400

    Recipes and Shopping List Components
    recipe and ingredient models

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'Delicious Recipe', 'https://img.delicious.com.au/52hfFIjf/h506-w759-cfill/del/2017/05/one-pot-butter-chicken-with-dill-yoghurt-46876-2.jpg'),
+    new Recipe('A test recipe', 'Delicious Recipe', 'https://img.delicious.com.au/52hfFIjf/h506-w759-cfill/del/2017/05/one-pot-butter-chicken-with-dill-yoghurt-46876-2.jpg')
+  ];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
